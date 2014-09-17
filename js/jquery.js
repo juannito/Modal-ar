22a7813cb61e05285bee618b5fea520d81be9478
Juannito <juannito@gmail.com>
2014-09-16 22:16:21 -0300

    full

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,50 @@
 $(document).ready(function(){
+
+    var opacity = $(".opacity");
+    var modal = $(".modal");
+    var boton = $(".boton");
+
+    // OCULTAR MODAL
+    modal.hide();
+    opacity.hide();
+
+    boton.click(function(){ 
+        opacity.fadeIn();
+        modal.fadeIn();
+    }); 
     
+    // MOSTRAR
+    opacity.click(function(){ 
+        $(this).fadeOut();
+        modal.fadeOut();
+    }); 
+    
+    // CENTRAR
+    
+    var ventanaAncho = $(window).width();
+    var ventanaAlto = $(window).height();
+    
+    var modalAncho = $(".modal").width();
+    var modalAlto = $(".modal").height();
+    
+//    var modalAncho = $(".modal").outerWidth();
+//    var modalAlto = $(".modal").outerHeight();
+    
+    var modalTop = (ventanaAlto-modalAlto)/2;
+    var modalLeft = (ventanaAncho-modalAncho)/2;
+    
+//    console.log(ventanaAncho);
+//    console.log(ventanaAlto);
+    
+    // MODAL
+    console.log(modalAncho);
+    console.log(modalAlto);
+    
+//    console.log(modalTop);
+//    console.log(modalLeft);
+    
+    modal.css("top", modalTop);
+    modal.css("left", modalLeft);
     
 
 });
